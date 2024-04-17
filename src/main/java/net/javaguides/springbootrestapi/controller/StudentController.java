def2b57a2417d45741752179ef8a79142641c8ef
Angelo Barcelos <angelorodrigues318@gmail.com>
2024-04-17 12:07:06 -0300

    Using RequesyMapping notation

--- src/main/java/net/javaguides/springbootrestapi/controller/StudentController.java
+++ src/main/java/net/javaguides/springbootrestapi/controller/StudentController.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,16 +11,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import net.javaguides.springbootrestapi.bean.Student;
 
 @RestController
+@RequestMapping("students")
 public class StudentController {
 
-    @GetMapping("student")
+    @GetMapping
     public ResponseEntity<Student> getStudent(){
         Student student = new Student(1, "Angelo", "Barcelos");
         //#1
@@ -34,7 +34,7 @@ public class StudentController {
                                 .body(student);
     }
 
-    @GetMapping("students")
+    @GetMapping
     public ResponseEntity<List<Student>> getStudents(){
         List<Student> students = new ArrayList<>();
         students.add(new Student(1, "Angelo", "Barcelos"));
@@ -45,7 +45,7 @@ public class StudentController {
         return ResponseEntity.ok(students);
     }
 
-    @GetMapping("students/{id}/{first-name}/{last-name}")
+    @GetMapping("{id}/{first-name}/{last-name}")
     public ResponseEntity<Student> studentPathVariable(@PathVariable("id") int studentId, 
                                         @PathVariable("first-name") String first, 
                                         @PathVariable("last-name") String last){
@@ -53,7 +53,7 @@ public class StudentController {
         return ResponseEntity.ok(student);
     }
 
-    @GetMapping("students/query")
+    @GetMapping("query")
     public ResponseEntity<Student> studentRequestVariable(@RequestParam int id,
                                             @RequestParam String firstName,
                                             @RequestParam String lastName){
@@ -61,7 +61,7 @@ public class StudentController {
         return ResponseEntity.ok(student);
     }
 
-    @PostMapping("students/create")
+    @PostMapping("create")
     public ResponseEntity<Student> createStudent(@RequestBody Student student){
         System.out.println(student.getId());
         System.out.println(student.getFirstName());
@@ -72,7 +72,7 @@ public class StudentController {
         return ResponseEntity.status(HttpStatus.CREATED).body(student);
     }
 
-    @PutMapping("students/{id}/update")
+    @PutMapping("{id}/update")
     public ResponseEntity<Student> updateStudent(@RequestBody Student student, @PathVariable int id){
         List<Student> students = (List<Student>) this.getStudents();
 
@@ -95,7 +95,7 @@ public class StudentController {
         return null;
     }
 
-    @DeleteMapping("students/{id}/delete")
+    @DeleteMapping("{id}/delete")
     public ResponseEntity<List<Student>> deleteStudent(@PathVariable int id) {
         List<Student> students = (List<Student>) this.getStudents();
     
